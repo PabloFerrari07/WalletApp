21c034843d4421e8d90431fbfacae512e8241ba0
Pablo <emanuelferrari011@gmail.com>
2025-07-14 16:40:50 -0300

    ISaldoService

---
+++ BilleteraApp/Services/ISaldoService.cs
@@ -0,0 +1,12 @@
+﻿using BilleteraApp.Dtos;
+
+namespace BilleteraApp.Services
+{
+    public interface ISaldoService
+    {
+        Task<SaldoDto> CargarSaldoAsync(int userId, SaldoDto dto);
+        Task<SaldoDto> ObtenerSaldoAsync(int userId);
+        Task<SaldoDto> ActualizarSaldoAsync(int userId, SaldoDto dto);
+        Task<bool> EliminarSaldoAsync(int userId);
+    }
+}
---
+++ BilleteraApp/Services/SaldoService.cs
@@ -0,0 +1,29 @@
+﻿using BilleteraApp.Dtos;
+
+namespace BilleteraApp.Services
+{
+    public class SaldoService : ISaldoService
+    {
+
+        
+        public async Task<SaldoDto> ActualizarSaldoAsync(int userId, SaldoDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<SaldoDto> CargarSaldoAsync(int userId, SaldoDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> EliminarSaldoAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<SaldoDto> ObtenerSaldoAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
